968f886031ca2904b523ac1bf6e395962bd7378c
joer1803 <joer1803@student.miun.se>
2019-09-04 20:04:26 +0200

    Klasser efter diagram

---
+++ HampesYatzy/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HampesYatzy
+{
+    class Player
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Nickname { get; set; }
+        public override string ToString()
+        {
+            return Nickname;
+        }
+    }
+}
