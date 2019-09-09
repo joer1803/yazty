6594197b086a2b3ecf56f56f769a9208d59a753e
joer1803 <joer1803@student.miun.se>
2019-09-09 21:23:45 +0200

    Gjort en metod för triss och 1or till 6or. Gjorde även en metod som väljer metod beroende på kategori(GetScore)

--- HampesYatzy/Yatzy.cs
+++ HampesYatzy/Yatzy.cs
@@ -23,7 +23,7 @@ namespace HampesYatzy
         public static int FullHouse;
         public static int Chance;
         public static bool IsYatzy;
-        
+
         //A method to keep track of total score
         //A method to keep track of upper score
         //A method to see if the player got 63 points to get bonus points
@@ -42,7 +42,7 @@ namespace HampesYatzy
         //      if( myDice[j].RollNumber == i )
         //        Count++;
 
-        //      if( Count > 4 )
+        //      if( Count == 5 )
         //        Sum = 50;
         //    }
         //  }
@@ -81,12 +81,63 @@ namespace HampesYatzy
 
         //  return Sum;
         //}
-        //public int CheckCategory()
-        //{
+        public int GetScore(int category, int[] dice)
+        {
+            switch (category) //väljer metod beroende på kategori. 1or - 6or är default
+            {
+                case 7:
+                    return CheckThreeOfAKind(dice);
+                //case 8:
+                //    return CheckFourOfAKind(dice);
+                //case 9:
+                //    return CheckSmallStraight();
+                //case 10:
+                //    return CheckLargeStraight();
+                //case 11:
+                //    return CheckFullHouse();
+                //case 12:
+                //    return CountChance();
+                //case 13:
+                //    return CheckYatzy();
+                default:
+                    return CountNumbers(category, dice);
 
-        //}
-
-
-
+                    
+                
+            }
+        }
+        private int CountNumbers(int category, int[] dice)
+        {
+            int sum = 0;
+            for(int i = 0; i < dice.Length; i++)
+            {
+                if (dice[i] == category)
+                {
+                    sum += category;
+                }
+            }
+            return sum;
+        }
+        private int CheckThreeOfAKind(int[] dice)
+        {
+            int sum = 0;
+            int countSame = 0;
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+            for (int i=0;i<=dice.Length;i++)
+            {
+                for(int j = 0; j <= numbers.Length; i++)
+                {
+                    if(dice[i] == numbers[j])
+                    {
+                        countSame++;
+                        if (countSame == 3)
+                        {
+                            sum = dice[i] * 3;
+                        }
+                    }
+                }
+            }
+            return sum;
+        }
     }
 }
