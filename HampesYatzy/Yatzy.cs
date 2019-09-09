3e5555d78858988f46ccafc4139fcd9608aca26e
Ida Gärdqvist <ida19891@hotmail.com>
2019-09-09 11:05:45 +0200

    Yatzy class. Added all the categories

--- HampesYatzy/Yatzy.cs
+++ HampesYatzy/Yatzy.cs
@@ -9,5 +9,22 @@ namespace HampesYatzy
     class Yatzy
     {
         public static int Ones;
+        public static int Twos;
+        public static int Threes;
+        public static int Fours;
+        public static int Sixes;
+        public static int OnePair;
+        public static int TwoPair;
+        public static int ThreeOfAKind;
+        public static int FourOfAKind;
+        public static int SmallStraight;
+        public static int BigStraight;
+        public static int FullHouse;
+        public static int Chance;
+        public static bool Yatzy;
+
+
+
+
     }
 }
