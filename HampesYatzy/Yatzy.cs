27a648aa965c1401cea26263b1dda7e427f6085a
Ida Gärdqvist <ida19891@hotmail.com>
2019-09-09 12:25:22 +0200

    Yatzy class. Lagt till lite metoder

--- HampesYatzy/Yatzy.cs
+++ HampesYatzy/Yatzy.cs
@@ -12,6 +12,7 @@ namespace HampesYatzy
         public static int Twos;
         public static int Threes;
         public static int Fours;
+        public static int Fives;
         public static int Sixes;
         public static int OnePair;
         public static int TwoPair;
@@ -22,6 +23,64 @@ namespace HampesYatzy
         public static int FullHouse;
         public static int Chance;
         public static bool Yatzy;
+        
+        //A method to keep track of total score
+        //A method to keep track of upper score
+        //A method to see if the player got 63 points to get bonus points
+
+
+
+        public int CalculateYahtzee( Dice[] myDice )
+        {
+          int Sum = 0;
+
+          for( int i = 1; i <= 6; i++ )
+          {
+            int Count = 0;
+            for( int j = 0; j < 5; j++ )
+            {
+              if( myDice[j].RollNumber == i )
+                Count++;
+
+              if( Count > 4 )
+                Sum = 50;
+            }
+          }
+
+          return Sum;
+        }
+
+
+
+
+        //Add dices when the same number is needed, the upper part of yatzy
+        public int AddUpDices(int DieNumber, Dice[] myDice)
+        {
+            int Sum = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (myDice[i].RollNumber == DieNumber)
+                {
+                    Sum += DieNumber;
+                }
+            }
+
+            return Sum;
+        }
+
+        //En metod som summerar tärningarnas värde om spelaren väljer att ta chansen
+        public int AddUpChance( Dice[] myDice )
+        {
+          int Sum = 0;
+
+          for( int i = 0; i < 5; i++ )
+          {
+            Sum += myDice[i].RollNumber;
+          }
+
+          return Sum;
+        }
 
 
 
