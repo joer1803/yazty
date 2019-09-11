5fb43907840dcd12c89c194567fe344cfd2bf024
Preifelt <hapr1800@student.miun.se>
2019-09-11 09:45:26 +0200

    Lagt till en meny som fungerar.

--- HampesYatzy/NewUser.xaml.cs
+++ HampesYatzy/NewUser.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 
 namespace HampesYatzy
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewUser.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewUser : Window
     {
-        public MainWindow()
+        public NewUser()
         {
             InitializeComponent();
-            //DiceTest();
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -36,21 +33,9 @@ namespace HampesYatzy
 
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
-            NewUser newUser = new NewUser();
-            newUser.Show();
+            Window window = new Window();
+            window.Show();
             this.Close();
         }
-
-        /*private void DiceTest()
-{
-   int nr = 1;
-   BitmapImage image = new BitmapImage(new Uri(@"Resources\d" + nr.ToString() + ".png", UriKind.Relative)); //skapar en bitmappsbild av en tärning
-   dice1.Source = image; //gör ovanstående bild till source till en tom image i gränssnittet
-   dice2.Source = image;
-   dice3.Source = image;
-   dice4.Source = image;
-   dice5.Source = image;
-}*/
-
     }
 }
---
+++ HampesYatzy/Rating.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HampesYatzy
+{
+    /// <summary>
+    /// Interaction logic for Rating.xaml
+    /// </summary>
+    public partial class Rating : Window
+    {
+        public Rating()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window();
+            window.Show();
+            this.Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Rating rating = new Rating();
+            rating.Show();
+            rating.Close();
+        }
+    }
+}
