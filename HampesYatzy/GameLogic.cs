cdce930e4cea014d38a2f3411e444e487d499db7
Preifelt <hapr1800@student.miun.se>
2019-09-13 16:01:55 +0200

    Gränssnittet fixat

--- HampesYatzy/GameLogic.cs
+++ HampesYatzy/GameLogic.cs
@@ -286,7 +286,7 @@ namespace HampesYatzy
         }
 
         private int BonusPoint()//Textblock or box for the total sum of 1-6 category goes inside the ()
-        {
+       {
             int sum = 0;
             int totalUp = //Textblock or box for the total sum of 1-6 category
             if (totalUp == 63 || totalUp > 63)
