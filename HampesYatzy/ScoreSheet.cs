295d2255b0518f365b240b65b1cac6f9b0db11d8
joer1803 <joer1803@student.miun.se>
2019-09-16 16:34:08 +0200

    gjorde bool array av kategorier

--- HampesYatzy/ScoreSheet.cs
+++ HampesYatzy/ScoreSheet.cs
@@ -8,21 +8,26 @@ namespace HampesYatzy
 {
     class ScoreSheet
     {
+        public ScoreSheet()
+        {
+            Categories = new bool[14];
+        }
+        public bool[] Categories { get; set; }
         public int Score { get; set; }
-        public bool Ones { get; set; }
-        public bool Twos { get; set; }
-        public bool Threes { get; set; }
-        public bool Fours { get; set; }
-        public bool Fives { get; set; }
-        public bool Sixes { get; set; }
-        public bool OnePair { get; set; }
-        public bool TwoPair { get; set; }
-        public bool ThreeOfAKind { get; set; }
-        public bool FourOfAKind { get; set; }
-        public bool SmallStraight { get; set; }
-        public bool BigStraight { get; set; }
-        public bool FullHouse { get; set; }
-        public bool Chance { get; set; }
-        public bool Yatzy { get; set; }
+        public int Ones { get; set; }
+        public int Twos { get; set; }
+        public int Threes { get; set; }
+        public int Fours { get; set; }
+        public int Fives { get; set; }
+        public int Sixes { get; set; }
+        public int OnePair { get; set; }
+        public int TwoPair { get; set; }
+        public int ThreeOfAKind { get; set; }
+        public int FourOfAKind { get; set; }
+        public int SmallStraight { get; set; }
+        public int BigStraight { get; set; }
+        public int FullHouse { get; set; }
+        public int Chance { get; set; }
+        public int Yatzy { get; set; }
     }
 }
