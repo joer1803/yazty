ca85b9157bac2d82f1bd3c1324bc7191331b4dce
joer1803 <joer1803@student.miun.se>
2019-09-19 00:23:38 +0200

    småfix

--- HampesYatzy/MainWindow.xaml.cs
+++ HampesYatzy/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -14,7 +15,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-
 namespace HampesYatzy
 {
     /// <summary>
@@ -25,44 +25,39 @@ namespace HampesYatzy
         public MainWindow()
         {
             InitializeComponent();
-
-            //DiceTest();
             GetFreePlayerList();
-            //GetBusyList();
         }
 
-        
         private void GetFreePlayerList()
         {
             lstAvailable.ItemsSource = null;
             lstAvailable.ItemsSource = DbOperations.GetFreePlayers();
         }
-        //private void GetBusyList()
-        //{
-        //    lstBusy.ItemsSource = null;
-        //    lstBusy.ItemsSource = DbOperations.GetInGamePlayers();
-        //}
-
-
         private void Btn_classic_Click(object sender, RoutedEventArgs e)
         {
             Play play = new Play(CreateNewGame(1));
             play.Show();
             this.Close();
         }
+
         private int CreateNewGame(int gameType)
         {
             List<Player> players = new List<Player>();
-            players.Add((Player)lstAvailable.SelectedItems);
+            for (int i = 0; i < lstAvailable.SelectedItems.Count; i++)
+            {
+                Player player = new Player();
+                player = (Player)lstAvailable.SelectedItems[i];
+                players.Add(player);
+            }
             return DbOperations.CreateGame(players, gameType);
         }
+
         private void Btn_steerd_Click(object sender, RoutedEventArgs e)
         {
             Play play = new Play(CreateNewGame(2));
             play.Show();
             this.Close();
         }
-
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -71,7 +66,6 @@ namespace HampesYatzy
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-
         }
 
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
@@ -87,19 +81,5 @@ namespace HampesYatzy
             newUser.Show();
             this.Close();
         }
-
-        /*private void DiceTest()
-{
-   int nr = 1;
-   BitmapImage image = new BitmapImage(new Uri(@"Resources\d" + nr.ToString() + ".png", UriKind.Relative)); //skapar en bitmappsbild av en tärning
-   dice1.Source = image; //gör ovanstående bild till source till en tom image i gränssnittet
-   dice2.Source = image;
-   dice3.Source = image;
-   dice4.Source = image;
-   dice5.Source = image;
-}*/
-
     }
-
-    
 }
